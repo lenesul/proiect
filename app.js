93ae61d0c1f592656bd12a915812419d100fb100
AlexB <lenesul@rocketmail.com>
2015-08-14 17:08:40 +0300

    comms

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 
 var express = require ("express");
 var morgan = require('morgan');
+var fs = require('fs');
 
 var app = express ();
 
@@ -11,7 +12,16 @@ app.use(morgan('dev'));
 app.use(express.static('public'));
 
 
-var server = app.listen(3000, function () {
+var obj;
+
+obj = JSON.parse(fs.readFileSync('database/data.json', 'utf8'));
+console.log(obj);
+
+app.get('/getdatabase', function(req, res){
+  res.send(obj);
+});
+
+var server = app.listen(443, function () {
 	// optional
 	var host = server.address().address;
 	var port = server.address().port;
